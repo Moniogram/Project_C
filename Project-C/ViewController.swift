917a7a221524587184867a209c8beb279bd7480b
Antonio <antoniomone97@gmail.com>
2018-06-18 22:04:57 +0200

    Initial Commit

---
+++ Project-C/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project-C
+//
+//  Created by Antonio Mone on 18/06/2018.
+//  Copyright © 2018 Antonio Mone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
